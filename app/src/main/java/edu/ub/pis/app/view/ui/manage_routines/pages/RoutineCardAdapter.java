ecff94e95c0222175944750d90617273ed24c6a5
OscarRomeral <oscarromeral00@gmail.com>
2023-05-04 20:08:30 +0200

    sha afegit popup a see routines

--- app/src/main/java/edu/ub/pis/app/view/ui/manage_routines/pages/RoutineCardAdapter.java
+++ app/src/main/java/edu/ub/pis/app/view/ui/manage_routines/pages/RoutineCardAdapter.java
@@ -1,12 +1,16 @@
 package edu.ub.pis.app.view.ui.manage_routines.pages;
 
+import android.content.Context;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.PopupWindow;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.squareup.picasso.Picasso;
@@ -15,11 +19,14 @@ import java.util.ArrayList;
 
 import edu.ub.pis.app.R;
 import edu.ub.pis.app.model.Routine;
+import edu.ub.pis.app.view.ui.manage_routines.ExercisesAdapter;
 
 public class RoutineCardAdapter extends RecyclerView.Adapter<RoutineCardAdapter.ViewHolder> {
     private ArrayList<Routine> mRoutines; //Referencia a la llista de rutines
     private OnClickInfoListener mOnClickInfoListener; // Qui hagi d'obrir popup amb info exercicis
 
+    private static RoutineCardAdapter instance;
+    private Context context;
     /** Definició de listener (interficie)
      *  per a quan algú vulgui escoltar un event de OnClickInfo, és a dir,
      *  quan l'usuari faci click en l'imatge info de la page AddRoutinesPage
@@ -28,8 +35,16 @@ public class RoutineCardAdapter extends RecyclerView.Adapter<RoutineCardAdapter.
         void OnClickInfo(int position);
     }
 
-    public RoutineCardAdapter(ArrayList<Routine> routineList) {
+    public RoutineCardAdapter(Context context, ArrayList<Routine> routineList) {
         this.mRoutines = routineList;
+        this.context = context;
+    }
+
+    public static RoutineCardAdapter getInstance(Context context, ArrayList<Routine> routineList) {
+        if (instance == null) {
+            instance = new RoutineCardAdapter(context, routineList);
+        }
+        return instance;
     }
 
     public void setOnClickInfoListener(OnClickInfoListener listener) {
@@ -54,7 +69,8 @@ public class RoutineCardAdapter extends RecyclerView.Adapter<RoutineCardAdapter.
         // El ViewHolder té el mètode que s'encarrega de llegir els atributs del User (1r parametre),
         // i assignar-los a les variables del ViewHolder.
         // Qualsevol listener que volguem posar a un item, ha d'entrar com a paràmetre extra (2n).
-        holder.bind(mRoutines.get(position), this.mOnClickInfoListener);
+        holder.bind(mRoutines.get(position), this.mOnClickInfoListener, context);
+
     }
 
     /**
@@ -93,7 +109,7 @@ public class RoutineCardAdapter extends RecyclerView.Adapter<RoutineCardAdapter.
             mInfoPicture = itemView.findViewById(R.id.info_icon);
         }
 
-        public void bind(final Routine routine, OnClickInfoListener Infolistener) {
+        public void bind(final Routine routine, OnClickInfoListener Infolistener, Context context) {
             mRoutineName.setText(routine.getName());
             // Seteja el listener onClick del botó d'amagar (hide), que alhora
             // cridi el mètode OnClickHide que implementen els nostres propis
@@ -101,7 +117,23 @@ public class RoutineCardAdapter extends RecyclerView.Adapter<RoutineCardAdapter.
             mInfoPicture.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    Infolistener.OnClickInfo(getAdapterPosition());
+                    // Crear la vista del Pop-up y la RecyclerView
+                    View popupView = LayoutInflater.from(context).inflate(R.layout.popup_routine_exercises_layout, null);
+                    RecyclerView recyclerView = popupView.findViewById(R.id.routine_exercises_recycler);
+
+                    // Crear un objeto PopupWindow
+                    PopupWindow popupWindow = new PopupWindow(popupView, ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT, true);
+
+                    // Configurar el adaptador y la lista de ejercicios en la RecyclerView
+                    ExercisesAdapter exercisesAdapter = new ExercisesAdapter(routine.getExercises());
+                    recyclerView.setLayoutManager(new LinearLayoutManager(context));
+                    recyclerView.setAdapter(exercisesAdapter);
+
+                    // Mostrar el Pop-up en la posición del botón de información
+                    int[] location = new int[2];
+                    mInfoPicture.getLocationOnScreen(location);
+                    popupWindow.showAtLocation(mInfoPicture, Gravity.NO_GRAVITY, location[0], location[1] - popupWindow.getHeight());
+
                 }
             });
         }
