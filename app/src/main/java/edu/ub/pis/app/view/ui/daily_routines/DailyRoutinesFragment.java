1e55ae881d5911ddacb78d18b3bee692eceeca55
HectorRecloud <hector@recloud.es>
2023-05-21 21:33:24 +0200

    Integrat amb tablayout del damian

--- app/src/main/java/edu/ub/pis/app/view/ui/daily_routines/DailyRoutinesFragment.java
+++ app/src/main/java/edu/ub/pis/app/view/ui/daily_routines/DailyRoutinesFragment.java
@@ -12,6 +12,7 @@ import androidx.viewpager.widget.ViewPager;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.tabs.TabLayout;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
@@ -39,6 +40,7 @@ public class DailyRoutinesFragment extends Fragment {
     private ViewPager pager;
     private ViewPagerAdapter pagerAdapter;
     private CircleIndicator indicator;
+    private TabLayout tabLayout;
 
     private FirebaseAuth mAuth = FirebaseAuth.getInstance();
     private QuerySnapshot totalRoutines;
@@ -51,8 +53,6 @@ public class DailyRoutinesFragment extends Fragment {
 
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
-        // Fer el fetch i passar les routines
 
         FirebaseUser currentUser = mAuth.getCurrentUser();
 
@@ -142,8 +142,8 @@ public class DailyRoutinesFragment extends Fragment {
         pagerAdapter = new ViewPagerAdapter(getChildFragmentManager(), fragments);
         pager.setAdapter(pagerAdapter);
 
-        indicator = binding.ciDaily;
-        indicator.setViewPager(pager);
+        tabLayout = binding.tabDaily;
+        tabLayout.setupWithViewPager(pager);
 
         LocalDate currentDate = LocalDate.now();
         DayOfWeek currentDayOfWeek = currentDate.getDayOfWeek();
@@ -151,16 +151,22 @@ public class DailyRoutinesFragment extends Fragment {
 
         pager.setCurrentItem(currentDayOfWeekValue);
 
-        //Metodo para hace clickables los indicadores
-        for (int i = 0; i < indicator.getChildCount(); i++) {
-            final int page = i;
-            indicator.getChildAt(i).setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    pager.setCurrentItem(page);
-                }
-            });
-        }
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                pager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
 
     }
 
