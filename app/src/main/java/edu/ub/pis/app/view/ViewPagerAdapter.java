0b4eeff096d4886d43360bb9af8449a285587add
HectorRecloud <hector@recloud.es>
2023-05-21 21:33:24 +0200

    Integrat amb tablayout del damian

--- app/src/main/java/edu/ub/pis/app/view/ViewPagerAdapter.java
+++ app/src/main/java/edu/ub/pis/app/view/ViewPagerAdapter.java
@@ -1,6 +1,7 @@
 package edu.ub.pis.app.view;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentStatePagerAdapter;
@@ -9,6 +10,7 @@ import java.util.ArrayList;
 
 public class ViewPagerAdapter extends FragmentStatePagerAdapter {
     private ArrayList<Fragment> fragments;
+    private String[] titles = new String[] {"MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"};
 
     public ViewPagerAdapter(FragmentManager fm, ArrayList<Fragment> fragments) {
         super(fm);
@@ -24,5 +26,11 @@ public class ViewPagerAdapter extends FragmentStatePagerAdapter {
     @Override
     public int getCount() {
         return fragments.size();
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return titles[position];
     }
 }
